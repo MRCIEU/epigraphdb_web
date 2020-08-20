9f18d2c094e763885885d67574848b56506503fa
YiLiu6240 <yi.liu6240@gmail.com>
2020-08-20 11:59:00 +0100

    Add checking for API connection in make check

--- backend/scripts/check.py
+++ backend/scripts/check.py
@@ -1,5 +1,6 @@
 from colorama import Fore, Style
 
+from app.utils import ping_endpoint
 from epigraphdb_common_utils import backend_env_configs, docker_web_env_configs
 
 
@@ -39,5 +40,27 @@ def check_env_configs() -> None:
     print(docker_web_env_configs.env_configs)
 
 
+def check_api_connection() -> None:
+    api_url = backend_env_configs.env_configs["api_url"]
+    api_status = ping_endpoint(f"{api_url}/ping")
+    if api_status:
+        api_status_str = (
+            Style.BRIGHT + Fore.GREEN + str(api_status) + Style.RESET_ALL
+        )
+    else:
+        api_status_str = (
+            Style.BRIGHT + Fore.RED + str(api_status) + Style.RESET_ALL
+        )
+
+    print(
+        Style.BRIGHT
+        + Fore.GREEN
+        + "\n# Check API connection"
+        + Style.RESET_ALL
+    )
+    print(f"epigraphdb_api: {api_url}\tconnected: {api_status_str}")
+
+
 if __name__ == "__main__":
     check_env_configs()
+    check_api_connection()
