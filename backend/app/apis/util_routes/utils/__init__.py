fd01c6835a3bedeba5ccc58b950ae3f618d8761e
YiLiu6240 <yi.liu6240@gmail.com>
2021-03-03 18:24:04 +0000

    Drop all es indices when drop all cache

--- backend/app/apis/util_routes/utils/__init__.py
+++ backend/app/apis/util_routes/utils/__init__.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 from fastapi import APIRouter
 
-from app.utils.database import mongo_epigraphdb_web
+from app.utils.database import es_client, mongo_epigraphdb_web
 
 router = APIRouter()
 
@@ -11,7 +11,9 @@ router = APIRouter()
 def get_utils_cache_drop(
     collection_name: Optional[str] = None, all: bool = False
 ) -> bool:
-    """Drop a mongodb collection, or every collection"""
+    """Drop a mongodb collection, or every collection.
+    When `all`, also drops all elasticsearch indices.
+    """
     all_coll_names = mongo_epigraphdb_web.list_collection_names()
     if collection_name is not None and not all:
         if collection_name in all_coll_names:
@@ -22,6 +24,9 @@ def get_utils_cache_drop(
     else:
         for collection_name in all_coll_names:
             mongo_epigraphdb_web[collection_name].drop()
+        es_indices: List[str] = list(es_client.indices.get_alias("*").keys())
+        for index in es_indices:
+            es_client.indices.delete(index=index, ignore=[400, 404])
         return True
 
 
