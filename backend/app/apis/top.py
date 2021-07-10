dab90b9f88a8d4940f439849d3141353e349b875
YiLiu6240 <yi.liu6240@gmail.com>
2021-07-10 12:53:00 +0100

    Add arg dependencies to /ping

--- backend/app/apis/top.py
+++ backend/app/apis/top.py
@@ -1,8 +1,21 @@
+import requests
 from fastapi import APIRouter
+
+from app.settings import api_url, neural_url
+from app.utils.database import es_client_connected, mongo_client_connected
 
 router = APIRouter()
 
 
-@router.get("/ping", summary="Ping backend API")
-async def get_ping() -> bool:
-    return True
+@router.get("/ping", response_model=bool, summary="Ping backend API")
+def get_ping(dependencies: bool = True) -> bool:
+    if not dependencies:
+        return True
+    else:
+        api_ok = requests.get(f"{api_url}/ping").ok
+        neural_ok = requests.get(f"{neural_url}/ping").ok
+        es_ok = es_client_connected()
+        mongo_ok = mongo_client_connected()
+        all = [api_ok, neural_ok, es_ok, mongo_ok]
+        all_ok = sum(all) == len(all)
+        return all_ok
