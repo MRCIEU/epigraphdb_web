1f921a3b6d8f9adb11035e06fa65855b9a80ad72
YiLiu6240 <yi.liu6240@gmail.com>
2022-04-13 09:11:42 +0100

    wip nlp

---
+++ frontend/src/components/NLP/OntologyCls.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div
+  div TODO
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "OntologyCls",
+  components: {
+    //
+  },
+  data() {
+    return {
+      //
+    }
+  },
+  computed: {
+    //
+  },
+  methods: {
+    //
+  },
+})
+</script>
---
+++ frontend/src/views/NLP/NLP.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+div
+  div
+    h3 NLP
+    p The natural language processing components for the EpiGraphDB platform
+  div
+    b-tabs(algin="center")
+      b-tab(title="Text embedding")
+        text-embedding
+      b-tab(title="Entity embedding")
+        entity-embedding
+      b-tab(title="Ontology classification")
+        ontology-cls
+</template>
+
+<script>
+import Vue from "vue";
+import EntityEmbedding from "@/components/NLP/EntityEmbedding.vue";
+import OntologyCls from "@/components/NLP/OntologyCls.vue";
+import TextEmbedding from "@/components/NLP/TextEmbedding.vue";
+
+export default Vue.extend({
+  name: "NLPView",
+  components: {
+    EntityEmbedding,
+    OntologyCls,
+    TextEmbedding,
+  },
+  data() {
+    return {
+      //
+    }
+  },
+  computed: {
+    //
+  },
+  methods: {
+    //
+  },
+})
+</script>
